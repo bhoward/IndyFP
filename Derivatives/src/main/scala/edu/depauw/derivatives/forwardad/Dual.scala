5ee183429cb3d26b8559dfedfe284fa6b451164b
Brian Howard <bhoward@depauw.edu>
2019-07-13 23:13:33 -0400

    Messing with formatting

--- Derivatives/src/main/scala/edu/depauw/derivatives/forwardad/Dual.scala
+++ Derivatives/src/main/scala/edu/depauw/derivatives/forwardad/Dual.scala
@@ -1,24 +1,35 @@
 package edu.depauw.derivatives.forwardad
 
 object Dual {
-case class Dual(value: Double, deriv: Double = 0)
+  case class Dual(value: Double, deriv: Double = 0)
 
-trait DualIsFractional extends Fractional[Dual] {
-  def plus(x: Dual, y: Dual): Dual = Dual(x.value + y.value, x.deriv + y.deriv)
-  def minus(x: Dual, y: Dual): Dual = Dual(x.value - y.value, x.deriv - y.deriv)
-  def times(x: Dual, y: Dual): Dual = Dual(x.value * y.value, x.deriv * y.value + x.value * y.deriv)
-  def div(x: Dual, y: Dual): Dual = Dual(x.value / y.value, (x.deriv * y.value - x.value * y.deriv) / (y.value * y.value))
-  def negate(x: Dual): Dual = Dual(-x.value, -x.deriv)
-  def fromInt(x: Int): Dual = Dual(x, 0)
-  def toInt(x: Dual): Int = x.value.toInt
-  def toLong(x: Dual): Long = x.value.toLong
-  def toFloat(x: Dual): Float = x.value.toFloat
-  def toDouble(x: Dual): Double = x.value
-}
+  trait DualIsFractional extends Fractional[Dual] {
+    def plus(x: Dual, y: Dual): Dual =
+      Dual(x.value + y.value, x.deriv + y.deriv)
+    def minus(x: Dual, y: Dual): Dual =
+      Dual(x.value - y.value, x.deriv - y.deriv)
+    def times(x: Dual, y: Dual): Dual =
+      Dual(x.value * y.value, x.deriv * y.value + x.value * y.deriv)
+    def div(x: Dual, y: Dual): Dual =
+      Dual(x.value / y.value, (x.deriv * y.value - x.value * y.deriv) / (y.value * y.value))
+    def negate(x: Dual): Dual =
+      Dual(-x.value, -x.deriv)
+    def fromInt(x: Int): Dual =
+      Dual(x, 0)
+    def toInt(x: Dual): Int =
+      x.value.toInt
+    def toLong(x: Dual): Long =
+      x.value.toLong
+    def toFloat(x: Dual): Float =
+      x.value.toFloat
+    def toDouble(x: Dual): Double =
+      x.value
+  }
 
-trait DualOrdering extends Ordering[Dual] {
-  def compare(x: Dual, y: Dual): Int = java.lang.Double.compare(x.value, y.value)
-}
+  trait DualOrdering extends Ordering[Dual] {
+    def compare(x: Dual, y: Dual): Int =
+      java.lang.Double.compare(x.value, y.value)
+  }
 
-implicit object DualIsFractional extends DualIsFractional with DualOrdering
+  implicit object DualIsFractional extends DualIsFractional with DualOrdering
 }
